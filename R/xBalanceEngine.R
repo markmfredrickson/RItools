fecb3ae414a9949f3f0879685be1c2666f1f6839
Josh Errickson <jerrick@umich.edu>
2013-12-10 11:52:56 -0500

    Whitespace cleanup

--- R/xBalanceEngine.R
+++ R/xBalanceEngine.R
@@ -52,9 +52,9 @@ xBalanceEngine <- function(ss,zz,mm,report, swt, s.p, normalize.weights,zzname,p
 
 	tmat <- (mm*swt$wtratio - msmn)
 
-        if (!is.null(post.align.trans))
-          tmat <- apply(tmat, 2, post.align.trans)
-        
+  if (!is.null(post.align.trans))
+    tmat <- apply(tmat, 2, post.align.trans)
+
 	dv <- unsplit(tapply(zz,ss,var), ##sample variance of treatment
                 ss)
 	ssvar <- apply(dv*tmat*tmat, 2, sum) ## for 1 column in  mm, sum(tmat*tmat)/(nrow(tmat)-1)==var(mm) and sum(dv*(mm-mean(mm))^2)=ssvar or wtsum*var(mm)
