c736b7af7d8dc8a4a0e27b687fdb1eeaaa9ee6a1
Ben B Hansen <bbh@umich.edu>
2016-04-01 20:32:58 -0400

    Fix for SparseM:::slm.wfit
    Also, moved SparseM-based utilities and SparseM fixes to utils.R, and added tests in test.utils.R

--- R/Design.R
+++ R/Design.R
@@ -402,41 +402,6 @@ designToDescriptives <- function(design, covariate.scaling = NULL) {
   return(ans)
 }
 
-##' Turn a factor variable into a sparse matrix of 0's and 1's, such that if observation i
-##' has the jth level then there is a 1 at position (i,j) (but nowhere else in row i).
-##'
-##' NA's give rise to rows with no 1s.
-##' As the result is only meaningful in the context of the SparseM package,
-##' function requires that SparseM be loaded.
-##' @title Sparse matrix dummy coding of a factor variable (omitting the intercept)
-##' @param thefactor Factor variable, or object inheriting from class factor
-##' @return Sparse csr matrix the columns of which are dummy variables for levels of thefactor
-##' @import SparseM
-##' @export
-##' @author Ben Hansen
-##' @examples
-##' sparse_mod_matrix <-  SparseMMFromFactor(iris$Species)
-##' mod_matrix <- model.matrix(~Species-1, iris)
-##' all.equal(as.matrix(sparse_mod_matrix),
-##'           mod_matrix, check.attributes=FALSE)
-SparseMMFromFactor <- function(thefactor) {
-  stopifnot(inherits(thefactor, "factor"))
-  theNA <- ##if (inherits(thefactor, "optmatch")) !matched(thefactor) else
-    is.na(thefactor)
-
-  if (all(theNA)) stop("No non-NA's in thefactor") 
-
-  nlev <- nlevels(thefactor)
-  nobs <- length(thefactor)
-  theint <- as.integer(thefactor)
-  if (any(theNA)) theint[theNA] <- 1L# odd; but note how we compensate in def of ra slot below
-  new("matrix.csr",
-      ja=theint,
-      ia=1L:(nobs+1L),
-      ra=(1L-theNA),
-      dimension = c(nobs, nlev) #+sum(theNA)
-      )
-}
 ##' Aggregate Design
 ##'
 ##' totals up all the covariates
@@ -628,36 +593,3 @@ sparseToVec <- function(s, column = TRUE) {
   }
 }
 
-##' slm.fit.csr with a fix
-##'
-##' SparseM's slm.fit.csr has a bug for intercept only models
-##' (admittedly, these are generally a little silly to be done as a
-##' sparse matrix), but in order to avoid duplicate code, if
-##' everything is in a single strata, we use the intercept only model.
-##' @param x As slm.fit.csr
-##' @param y As slm.fit.csr
-##' @param ... As slm.fit.csr
-##' @return As slm.fit.csr
-##' @import SparseM
-slm.fit.csr.fixed <- function (x, y, ...)
-{
-    if (is.matrix(y))
-        n <- dim(y)[1]
-    else n <- length(y)
-    p <- x@dimension[2]
-    if (n != x@dimension[1])
-        stop("x and y don't match n")
-    chol <- SparseM::chol(t(x) %*% x, ...)
-    xy <- t(x) %*% y
-    coef <- SparseM::backsolve(chol, xy)
-
-    if (is.vector(coef)) {
-      coef <- matrix(coef, ncol = dim(y)[2], nrow = p)
-    }
-
-    fitted <- as.matrix(x %*% coef)
-    resid <- y - fitted
-    df <- n - p
-    list(coefficients = coef, chol = chol, residuals = resid,
-        fitted = fitted, df.residual = df)
-}
--- tests/testthat/test.utils.R
+++ tests/testthat/test.utils.R
@@ -75,3 +75,69 @@ test_that("Select a subset of xbal results (for printing, etc)", {
 
 
 })
+
+
+test_that("fitter for sparse designs handles intercept only design",
+          {
+              expect_true(require("SparseM"))
+              nullfac <- factor(rep("a", 4))
+              nullfac.csr <- SparseMMFromFactor(nullfac)
+              quickY <- 1:4
+              quickY.mat <- matrix(quickY)
+              quickY.csr <- as(quickY.mat, "matrix.csr")
+              expect_equivalent(SparseM::slm.fit.csr(nullfac.csr, quickY), # i.e., no problem
+                                SparseM::slm.fit.csr(nullfac.csr, quickY.mat) # w/ these y classes
+                                )
+              expect_error(SparseM::slm.fit.csr(nullfac.csr, quickY.csr)) # when this ceases,
+                                        # we get to revert to SparseM:slm.fit.csr
+              lm.n <- lm.fit(matrix(1,4,1), quickY)
+
+              slm.n1 <- slm.fit.csr.fixed(nullfac.csr, quickY)
+
+              expect_equal(lm.n$fitted, as.vector(slm.n1$fitted))
+              expect_equal(lm.n$residuals, as.vector(slm.n1$residuals))
+          }
+          )
+
+test_that("Residuals from weighted regressions w/ sparse designs",
+          {
+              nullfac <- factor(rep("a", 4))
+              nullfac.csr <- SparseMMFromFactor(nullfac)
+
+              quickfac <- factor(rep(letters[1:2], each=2))
+              quickfac.csr <- SparseMMFromFactor(quickfac)
+              quickY <- 1:4
+
+              ## First, document the problem.
+              slm.u <- SparseM:::slm.fit(quickfac.csr, quickY)
+              slm.w <- SparseM:::slm.wfit(quickfac.csr, quickY,
+                                         weights=rep(1:2, each=2)
+                                         )
+              expect_equal(coef(slm.u), # this 
+                           coef(slm.w)) # is OK...
+
+              ## but since SparseM:::slm.wfit() doesn't compensate for weighting of design matrix:
+              expect_false(all(abs(slm.u$resid - slm.w$resid) < .Machine$double.eps^.5))
+              expect_false(all(abs(slm.u$fitted - slm.w$fitted) < .Machine$double.eps^.5))
+
+              ## Now for the fix
+              ## First, example from `lm.wfit`:
+              set.seed(129)
+              
+              n <- 7 ; p <- 2
+              X <- matrix(rnorm(n * p), n, p) # no intercept!
+              y <- rnorm(n)
+              w <- rnorm(n)^2
+     
+              lmw <- lm.wfit(x = X, y = y, w = w)
+
+              X.csr <- as(X, "matrix.csr")
+
+              slmw <- slm.wfit.csr(x=X.csr, y=y, weights=w)
+
+              expect_equal(coef(lmw), as.vector(coef(slmw)), check.attributes=FALSE) # OK here...
+
+              expect_equal(lmw$fitted, as.vector(slmw$fitted)) # OK here 
+              expect_equal(lmw$residuals, as.vector(slmw$residuals)) # also
+
+          })
