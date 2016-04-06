4460b30fb13630edf5c62f0eaadd6b8f2f9f938f
Ben B Hansen <bbh@umich.edu>
2016-04-06 07:16:07 -0400

    Simple tests of xBal's adj.diff, z.score against base R reference fcts

--- tests/testthat/test.xBalance.R
+++ tests/testthat/test.xBalance.R
@@ -7,6 +7,45 @@ library("testthat")
 
 context("xBalance Functions")
 
+test_that("xBal univariate desriptive means agree w/ lm",{
+    set.seed(20160406)
+    n <- 7 
+     dat <- data.frame(x1=rnorm(n), x2=rnorm(n),
+                        s=rep(c("a", "b"), c(floor(n/2), ceiling(n/2)))
+                        )
+     dat = transform(dat, z=as.numeric( (x1+x2+rnorm(n))>0 ) )
+
+     lm1 <- lm(x1~z, data=dat)
+     xb1 <- xBalance(z~x1+strata(s), data=dat, report=c("adj.mean.diffs"))
+     expect_equal(xb1$results["x1", "adj.diff", "Unstrat"], coef(lm1)["z"], check.attributes=F)
+
+     ## try to match default ETT weighting
+     pihat <- fitted(lm(z~s, data=dat))     
+     lm2a <- lm(x1~z+s, data=dat, weights=ifelse(pihat==1,1, (1-pihat)^-1))
+
+     expect_equal(xb1$results["x1", "adj.diff", "s"], coef(lm2a)["z"], check.attributes=F)
+
+
+})
+
+test_that("xBal univariate inferentials agree w/ conditional logistic Rao score test",{
+    library(survival)
+    set.seed(20160406)
+    n <- 7 
+     dat <- data.frame(x1=rnorm(n), x2=rnorm(n),
+                        s=rep(c("a", "b"), c(floor(n/2), ceiling(n/2)))
+                        )
+     dat = transform(dat, z=as.numeric( (x1+x2+rnorm(n))>0 ) )
+    xb1b <- xBalance(z~x1+strata(s), data=dat, report=c("z.scores"))
+     cl1 <- clogit(z~x1, data=dat)
+     cl2 <- clogit(z~x1+strata(s), data=dat)
+
+
+    expect_equal(summary(cl1)$sctest['test'],(xb1b$results["x1", "z", "Unstrat"])^2 , check.attributes=F)
+    expect_equal(summary(cl2)$sctest['test'],(xb1b$results["x1", "z", "s"])^2 , check.attributes=F)
+}
+          )
+
 test_that("xBalance returns covariance of tests", {
   set.seed(20130801)
   n <- 500
