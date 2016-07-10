2db38f9b5d94c6045299b4d0e2eab562c17db29e
Ben Hansen <bbh@umich.edu>
2016-07-10 12:10:30 -0400

    Inverse probability of assignment weighting
    For RCTs that are completely randomized within the block.

---
+++ tests/testthat/test.ipw.R
@@ -0,0 +1,16 @@
+
+library('testthat')
+context('Inverse assignment weighting')
+
+test_that("Assignment weights properly inferred from treatment assmts by stratum",
+          {
+tx1 <- c(1,0,1,0,0)
+fac1 <- rep(letters[1:2], c(2,3))
+fac1 <- as.factor(fac1)
+expect_equivalent(ipwts_blockRCT(tx1,fac1), c(2,2,3,1.5,1.5))
+expect_equivalent(ipwts_blockRCT(tx1,as.character(fac1)), c(2,2,3,1.5,1.5))
+}   )
+
+## To-dos:
+## - test cluster arg
+## - test dropping of blocks in which not all conditions are represented
