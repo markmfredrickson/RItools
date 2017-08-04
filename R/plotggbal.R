f2caf76290584a839332b424e739e409ffb32481
Josh Errickson <josh@errickson.net>
2017-08-04 12:37:14 -0400

    Some preliminary work at 'grouping'

--- R/plotggbal.R
+++ R/plotggbal.R
@@ -10,7 +10,8 @@
 #' @param variable.labels Same as plot.xbal
 #' @param legend.position Where the legend should go ("bottom", "right")
 #' @param legend.title Title of legend (blank by default)
-#' @param var.order List of variable names (variable.labels if non-NULL) defining ordering. Otherwise alphabetical.
+#' @param var.order Vector of variable names (variable.labels if non-NULL) defining ordering. Otherwise alphabetical. Only one of `var.order` and `var.groupings` can be non-NULL.
+#' @param var.groupings List of named vectors of variables names (variable.labels if non-NULL) which should be grouped. The name of the list entry is the grouping label. Only one of `var.order` and `var.groupings` can be non-NULL.
 #' @param ...
 #'
 #' @export
@@ -30,10 +31,12 @@ plotggxbal <- function(x,
                        legend.position = "bottom",
                        legend.title = "",
                        var.order = NULL,
+                       var.grouping = NULL,
                        ...) {
 
+  stopifnot(is.null(var.order) | is.null(var.grouping))
 
-  x <- as.data.frame(prepareXbalForPlot(x, statistic, absolute,
+  x <- as.data.frame(RItools:::prepareXbalForPlot(x, statistic, absolute,
                                         strata.labels, variable.labels))
 
   # Tidyverse doesn't like rownames
@@ -56,6 +59,20 @@ plotggxbal <- function(x,
                                       decreasing = TRUE))
   }
 
+  if (!is.null(var.grouping)) {
+    for (grouplabel in names(var.grouping)) {
+      curorder <- levels(x$rowname)
+      neworder <- curorder[!(curorder %in% var.grouping[[grouplabel]])]
+      neworder <- c(rev(var.grouping[[grouplabel]]), grouplabel, neworder)
+      x$rowname <- factor(x$rowname, levels = neworder)
+      for (stratas in unique(x$strata)) {
+        x <- rbind(x, c(grouplabel, stratas, NA))
+      }
+      x$values <- as.numeric(x$values)
+    }
+  }
+
+
 
 
 
@@ -65,7 +82,7 @@ plotggxbal <- function(x,
                                        color = strata,
                                        shape = strata)) +
     ggplot2::geom_vline(xintercept = 0) +
-    ggplot2::geom_line(aes(group = rowname), color = "black") +
+    ggplot2::geom_line(ggplot2::aes(group = rowname), color = "black") +
     ggplot2::geom_point() +
     ggplot2::theme(legend.position = legend.position) +
     ggplot2::labs(color = legend.title,
