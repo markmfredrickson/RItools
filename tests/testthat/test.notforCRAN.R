22fa7a0a3134ed3f1052b68432b2ff5cf873391a
Ben Hansen <bbh@umich.edu>
2018-08-03 14:18:05 -0400

    Verify expectation re survival package
    Closes #72

--- tests/testthat/test.notforCRAN.R
+++ tests/testthat/test.notforCRAN.R
@@ -4,6 +4,8 @@
 
 ###    tests relocated from test.utils.R      ###
 
+context("Utilities, external dependencies etc")
+
 test_that("data.table options issue #69", {
 
   if (suppressMessages(suppressWarnings(require(data.table)))) {
@@ -12,4 +14,12 @@ test_that("data.table options issue #69", {
     expect_equal(withOptions(list(), f), 1)
   }
 
+})
+
+test_that("survival::strata() still conforms to our expectations",
+{
+    facA <- rep(c("a", "b", "c"), each=2)
+    facB <- rep(c("A", "B", "C"), 2)
+    facC <- survival::strata(facA, facB)
+    expect_equal(6, nlevels(facC))
 })
