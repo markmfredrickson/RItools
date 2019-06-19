c1fffdccfe61d0b8299d09a77c8a1a3ef6ecff01
Jake Bowers <jake@jakebowers.org>
2019-06-19 10:52:02 -0700

    Fixing tests that were causing check errors and warnings

--- tests/testthat/test.Design.R
+++ tests/testthat/test.Design.R
@@ -86,6 +86,7 @@ test_that("lookup tables OK, even w/ complex & multi-column terms",{
 })
 
 test_that("Issue #76: Using I() in formulas", {
+  set.seed(12345)
   x <- data.frame(x = rnorm(10), y = rnorm(10), z = rbinom(10, size = 1, p = 1/3))
   x$"(weights)" <- 1
   d <- makeDesigns(z ~ I(x * sin(y)), data = x)
