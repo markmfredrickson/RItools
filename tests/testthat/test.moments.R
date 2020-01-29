29495da7ef0452ada916f02be2cbdf2bfdfa3cc1
Mark M. Fredrickson <mark.m.fredrickson@gmail.com>
2020-01-29 10:52:42 -0500

    adding moment tests for the balanceTest function

--- tests/testthat/test.moments.R
+++ tests/testthat/test.moments.R
@@ -4,6 +4,7 @@ context('Moment Calculation')
 test_that("Calculating moments of Mahalanobis statistic", {
     library(MASS)
 
+    ### Set up
     ## Generate some random data
     set.seed(30303)
     n <- 12
@@ -22,6 +23,7 @@ test_that("Calculating moments of Mahalanobis statistic", {
               0, 1, 0,
               0, 1, 0, 1, 1)
 
+    ### end data set up
     x <- model.matrix(~ x1 + x2 + x3 - 1, data = df)
     s <- model.matrix(~ match - 1, data = df)
 
@@ -35,75 +37,68 @@ test_that("Calculating moments of Mahalanobis statistic", {
     one_p <- sn0 / sn # 1 - pi
     p_one_p <- p * one_p # pi (1 - pi)
 
-    ## The E(JJ')
+    ## The E(JJ') matrix
     Gamma <- s %*% diag((p * (sn1 - 1) / (sn - 1) - p^2) / (p^2 * one_p^2))  %*% t(s)
     diag(Gamma) <- 1 / as.vector(s %*% p_one_p)
 
     ## now combine Gamma with x get the central matrix of the treatment assignment sandwich
     x_svd <- svd(x)
-    u <- x_svd$u[, x_svd$d >= sqrt(.Machine$double.eps)]
+    u <- x_svd$u[, x_svd$d > sqrt(.Machine$double.eps)]
 
-    G_inv <- ginv(Gamma)
+    ## meat in the J' a J sandwich
+    meat <- u %*% ginv(t(u) %*% Gamma %*% u) %*% t(u)
 
-    uuG_uu <- u %*% t(u) %*% G_inv %*% u %*% t(u)
+    ZtoJ <- function(z) { (z - s %*% p) / (s %*% p_one_p) }
 
     ## Compute the Mahalanobis distance
     mahal <- function(z) {
-        j <- (z - s %*% p) / (s %*% p_one_p)
-        (t(j) %*% uuG_uu %*% j)[1,1]
+        j <- ZtoJ(z)
+        (t(j) %*% meat  %*% j)[1,1]
     }
-
-    mahal2 <- function(z) {
-        j <- (z - s %*% p) / (s %*% p_one_p)
-        (t(j) %*% x %*% ginv(t(x) %*% Gamma %*% x) %*% t(x) %*% j)[1,1]
-    }
-
-    ref_xb_chisquare <- xBalance(z ~ x1 + x2 + x3, data = df, strata = list(a = ~ match), report = 'all')$overall$chisquare
-    expect_equal(mahal(df$z),
-                 ref_xb_chisquare)
-
-    expect_lte((mahal2(df$z) - ref_xb_chisquare)^2, sqrt(.Machine$double.eps))
 
     ## generate all possible randomizations
     zs_by_strata <- mapply(sn, sn1, FUN = function(n, n1) {
-       apply(combn(n, n1), 2, function(idx) {
+        apply(combn(n, n1), 2, function(idx) {
             zz <- numeric(n)
             zz[idx] <- 1
             return(zz)
         })
     })
 
-    js_by_strata <- mapply(1:nlevels(df$match), zs_by_strata, FUN = function(id, zs) {
-        js <- (zs - p[id]) / p_one_p[id]
-    })
+    zj <- sapply(zs_by_strata, ncol)
+    idxes <- as.matrix(do.call(expand.grid, lapply(zj, function(k) { 1:k })))
+    zs <- apply(idxes, 1, function(idx) {
+        unlist(mapply(idx, zs_by_strata, FUN = function(a, b) { b[, a] }))})
 
-    nj <- sapply(js_by_strata, ncol)
-    idxes <- as.matrix(do.call(expand.grid, lapply(nj, function(k) { 1:k })))
 
-    ms <- apply(idxes, 1, function(idx) {
-        ## BEWARE makes strong assumption uuG_uu is in strata sorted order
-        j <- unlist(mapply(idx, js_by_strata, FUN = function(a, b) { b[, a]}))
-        t(j) %*% uuG_uu %*% j
-    })
+    ref_xb_chisquare <- xBalance(z ~ x1 + x2 + x3, data = df, strata = list(a = ~ match), report = 'all')$overall$chisquare
+    expect_equal(mahal(df$z),
+                 ref_xb_chisquare)
 
+
+    ms <- apply(zs, 2, mahal)
     expect_equal(mean(ms), 4)
 
-    xbs <- apply(idxes, 1, function(idx) {
+    xbs <- apply(zs, 2, function(z) {
         tmp <- df
-        ## Same assumption as above
-        tmp$z <- unlist(mapply(idx, zs_by_strata, FUN = function(a, b) { b[, a]}))
+        tmp$z <- z
         xb <- xBalance(z ~ x1 + x2 + x3, data = tmp, strata = list(a = ~ match), report = 'all')
         xb$overall$chisquare
     })
 
     expect_equal(mean(xbs), 4)
 
-    ms2 <- apply(idxes, 1, function(idx) {
-        z <- unlist(mapply(idx, zs_by_strata, FUN = function(a, b) { b[, a]}))
-        mahal2(z)
+    expect_true(all((ms - xbs)^2 <= sqrt(.Machine$double.eps)))
+
+
+    bts <- apply(zs, 2, function(z) {
+        tmp <-df
+        tmp$z <- z
+        bt <- balanceTest(z ~ x1 + x2 + x3 + strata(match), data = tmp, report = 'all')$overall[1,1]
     })
 
-    expect_equal(mean(ms2), 4)
+    expect_equal(mean(bts), 4)
 
-    expect_true(all((ms2 - xbs)^2 <= sqrt(.Machine$double.eps)))
+    expect_true(all((ms - bts)^2 <= sqrt(.Machine$double.eps)))
+    expect_true(all((xbs - bts)^2 <= sqrt(.Machine$double.eps)))
 })
