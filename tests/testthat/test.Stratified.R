f52f648caab5e14532d50b6a14b1cffe4ff0126d
Mark M. Fredrickson <mark.m.fredrickson@gmail.com>
2020-06-15 14:52:33 -0400

    moving empirical moment test helper code into own file

--- tests/testthat/test.Stratified.R
+++ tests/testthat/test.Stratified.R
@@ -2,31 +2,7 @@ library("testthat")
 
 context("Stratified Designs")
 
-## A copy of this also exists in test.Stratified.R
-## TODO: Move them to own utils.R file
-
-## @param s A n x s stratum membership matrix
-## @param sn A s-vector of units per stratum
-## @param sn1 A s-vector of treated units per stratum
-## @return A matrix of treatment assignments at the unit level
-design_to_zs <- function(s, sn, sn1) {
-
-    ## generate all possible randomizations
-    zs_by_strata <- mapply(sn, sn1, FUN = function(n, n1) {
-        apply(combn(n, n1), 2, function(idx) {
-            zz <- numeric(n)
-            zz[idx] <- 1
-            return(zz)
-        })
-    }, SIMPLIFY = FALSE)
-
-    zj <- sapply(zs_by_strata, ncol)
-    idxes <- as.matrix(do.call(expand.grid, lapply(zj, function(k) { 1:k })))
-    zs <- apply(idxes, 1, function(idx) {
-        unlist(mapply(idx, zs_by_strata, FUN = function(a, b) { b[, a] }))})
-
-    return(zs)
-}
+source("misc.R")
 
 test_that("Rotated covariates", {
     n <- 12 
@@ -44,33 +20,6 @@ test_that("Rotated covariates", {
     rotx <- rotate_covariates(d, x)
 
     expect_equal(dim(rotx), c(12, 3))
-
-    ## a version of this function also appears in test.moments.R
-    empirical_cov <- function(x, s, sn, sn1) {
-        sn0 <- sn - sn1
-
-        zs <- design_to_zs(s, sn, sn1)
-        k <- dim(zs)[2]
-
-        ## strata level probabilities
-        p <- sn1 / sn # pi
-
-        ## j = Z - E(Z)
-        ZtoJ <- function(z) { (z - s %*% p) }
-
-        ## Compute the Mahalanobis distance
-        xj <- function(z) {
-            j <- ZtoJ(z)
-            t(x) %*% j
-        }
-
-        xjs <- apply(zs, 2, xj)
-
-        cov_xj <- (k - 1) / k * cov(t(xjs))
-
-        return(cov_xj)
-    }
-
 
     emp_cov <- empirical_cov(x, d@Units, d@Count, d@Treated)
     emp_rot <- XtX_pseudoinv_sqrt(emp_cov, TRUE)
