8394238eb75fe72eb43931a694f2ed482239c1fa
Josh Errickson <josh@errickson.net>
2023-02-06 09:57:45 -0500

    GGplot deprecations should be caught better by testing suite #130

--- tests/testthat/test.plot.balancetest.R
+++ tests/testthat/test.plot.balancetest.R
@@ -16,5 +16,23 @@ test_that("Uses ggplot", {
   bt <- balanceTest(Z ~ X1 + X2 + X3 + X4 + strata(s), data = df)
   btp <- plot(bt)
   expect_is(btp, "gg")
+  btp
 
 })
+
+
+if (requireNamespace("optmatch", quietly = TRUE)) {
+  test_that("plotting with nuclearplants", {
+    data(nuclearplants)
+    psm <- glm(pr ~ date + t1 + t2 + cap + ne + ct + bw + cum.n + pt,
+           family = binomial, data = nuclearplants)
+    psm.dist <- optmatch::match_on(psm, data=nuclearplants)
+    ps.pm2 <- optmatch::pairmatch(psm.dist, data = nuclearplants)
+    myb <- balanceTest(pr ~ date + t1 + t2 + cap + ne + ct + bw + cum.n +
+                         strata(ps.pm2), data = nuclearplants)
+    p <- plot(myb)
+    expect_is(p, "gg")
+    p
+
+  })
+}
